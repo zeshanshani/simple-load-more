2d6e2052869e302d01c90f689749a0422d0058b0
Zeshan Ahmed <me@zeshanahmed.com>
2019-02-23 23:04:00 +0500

    Publish 1.0.1 version
    Signed-off-by: Zeshan Ahmed <me@zeshanahmed.com>

--- jquery.simpleLoadMore.js
+++ jquery.simpleLoadMore.js
@@ -1,7 +1,7 @@
 /**
  * Simple Load More
  *
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Zeshan Ahmed
  * Website: https://zeshanahmed.com/
  * Github: https://github.com/zeshanshani/simple-load-more/
