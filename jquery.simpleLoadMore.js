4dd3e9ddbf89b2935aae6ca33b1a07b95c57c187
Zeshan Ahmed <me@zeshanahmed.com>
2019-02-27 05:26:00 +0500

    Version 1.0.2
    This version fixes the issue where multiple elements targeting using the same class were considered as single element.
    
    You can now apply a generic class like “.simple-load-more” and use this class on multiple lists. All the lists will be treated as separate elements.
    
    Signed-off-by: Zeshan Ahmed <me@zeshanahmed.com>

--- jquery.simpleLoadMore.js
+++ jquery.simpleLoadMore.js
@@ -1,7 +1,7 @@
 /**
  * Simple Load More
  *
- * Version: 1.0.1
+ * Version: 1.0.2
  * Author: Zeshan Ahmed
  * Website: https://zeshanahmed.com/
  * Github: https://github.com/zeshanshani/simple-load-more/
@@ -17,46 +17,54 @@
 
     // Variables
     var $loadMore = $(this);
-    var $items = $loadMore.find(settings.item);
-    var btnHTML = settings.btnHTML ? settings.btnHTML : '<a href="#" class="load-more__btn">View More <i class="fas fa-angle-down"></i></a>';
-    var $btnHTML = $(btnHTML);
 
-    // Add classes.
-    $loadMore.addClass('load-more');
-    $items.addClass('load-more__item');
+    // Run through all the elements.
+    $loadMore.each(function(i, el) {
 
-    // Add button.
-    if ( ! $loadMore.find( '.load-more__btn' ).length && $items.length > 5 ) {
-      $loadMore.append( $btnHTML );
-    }
+      // Variables.
+      var $thisLoadMore = $(this);
+      var $items        = $thisLoadMore.find(settings.item);
+      var btnHTML       = settings.btnHTML ? settings.btnHTML : '<a href="#" class="load-more__btn">View More <i class="fas fa-angle-down"></i></a>';
+      var $btnHTML      = $(btnHTML);
 
-    $btn = $loadMore.find( '.load-more__btn' );
+      // Add classes
+      $thisLoadMore.addClass('load-more');
+      $items.addClass('load-more__item');
 
-    if ( ! $btn.length ) {
-      $btn = $btnHTML;
-    }
-
-    if ( $items.length > settings.count ) {
-      $items.slice(settings.count).hide();
-    }
-
-    // Add click event on button.
-    $btn.on('click', function(e) {
-      e.preventDefault();
-
-      var $this = $(this),
-      $updatedItems = $items.filter(':hidden').slice(0, settings.count);
-
-      // Show the selected elements.
-      if ( $updatedItems.length > 0 ) {
-        $updatedItems.fadeIn();
+      // Add button.
+      if ( ! $thisLoadMore.find( '.load-more__btn' ).length && $items.length > settings.count ) {
+        $thisLoadMore.append( $btnHTML );
       }
 
-      // Hide the 'View More' button
-      // if the elements lenght is less than 5.
-      if ( $updatedItems.length < settings.count ) {
-        $this.remove();
+      $btn = $thisLoadMore.find( '.load-more__btn' );
+
+      // Check if button is not present. If not, then attach $btnHTML to the $btn variable.
+      if ( ! $btn.length ) {
+        $btn = $btnHTML;
       }
+
+      if ( $items.length > settings.count ) {
+        $items.slice(settings.count).hide();
+      }
+
+      // Add click event on button.
+      $btn.on('click', function(e) {
+        e.preventDefault();
+
+        var $this = $(this),
+        $updatedItems = $items.filter(':hidden').slice(0, settings.count);
+
+        // Show the selected elements.
+        if ( $updatedItems.length > 0 ) {
+          $updatedItems.fadeIn();
+        }
+
+        // Hide the 'View More' button
+        // if the elements lenght is less than 5.
+        if ( $updatedItems.length < settings.count ) {
+          $this.remove();
+        }
+      });
     });
   }
 }( jQuery ));
