7cd4fa7146818543d9e1beb167a31a9c56b321d0
Zeshan Ahmed <premiumsrapidshares@gmail.com>
2019-04-24 13:26:38 +0500

    Fix script location in demo.html

--- jquery.simpleLoadMore.js
+++ jquery.simpleLoadMore.js
@@ -1,7 +1,7 @@
 /**
  * Simple Load More
  *
- * Version: 1.2.4
+ * Version: 1.2.5
  * Author: Zeshan Ahmed
  * Website: https://zeshanahmed.com/
  * Github: https://github.com/zeshanshani/simple-load-more/
